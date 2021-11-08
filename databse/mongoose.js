8352b978a29ef64222b4a853ede2cc97a40d639c
unknown <deepak1233s456@gmail.com>
2021-11-08 05:34:51 -0800

    db changed

--- databse/mongoose.js
+++ databse/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/image-gallery', {
-	useNewUrlParser: true
-}).then(() => {
-	console.log('database is connected')
-}).catch((err) => {
-	console.log('Error connecting to the database', err)
-})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/image-gallery', { useNewUrlParser: true })
+        .then(connect => console.log('connected to mongodb..'))
+        .catch(e => console.log('could not connect to mongodb', e))
+
+module.exports = {mongoose}
